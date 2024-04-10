df9f5988733c0fa3f4697f58c2343c48ba0c3869
Shane Coutinho <coutinhoshane7@gmail.com>
2024-04-10 22:57:18 +0200

    Convert player and computer to lowercase

--- javascript.js
+++ javascript.js
@@ -3,3 +3,9 @@ function getComputerChoice(){
     let random_element = plays[Math.floor(Math.random()* plays.length)] 
     return random_element
 }
+console.log(getComputerChoice())
+
+function playRound(playerSelection,ComputerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+}
